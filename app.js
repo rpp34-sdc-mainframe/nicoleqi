e2b822695df5dc78ce47614b20c18d2cf5fea0cf
nicoleqi7 <nicole.q.daddy@gmail.com>
2022-06-09 08:40:18 -0700

    test finished

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var productRouter = require('./routes/products');
-var cartRouter = require('./routes/carts');
 var app = express();
 var bodyParser = require("body-parser");
 const errors = require("express-async-errors");
 
 app.use('/products', productRouter);
-app.use('/carts', bodyParser.json(), cartRouter);
 
 /**
  * 全局异常处理
